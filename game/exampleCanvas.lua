80c3a6fe78459f181ff97837188663896e5adbb3
pisayesiwsi <152765566+pisayesiwsi@users.noreply.github.com>
2025-06-14 12:16:37 -0400

    Text example

--- game/exampleCanvas.lua
+++ game/exampleCanvas.lua
@@ -4,6 +4,10 @@ local ExampleCanvas = Canvas:extend({
 
 function ExampleCanvas:init()
 	Log.info("Initialised "..tostring(ExampleCanvas._name))
+	local test = TextDisplay:new(0, 0, "dövey2d is an extremely experimental\nextension for LÖVE2D!")
+		:setFont(love.graphics.newFont(24))
+		:setAlignment("CENTER")
+	self:add(test)
 	return self
 end
 
